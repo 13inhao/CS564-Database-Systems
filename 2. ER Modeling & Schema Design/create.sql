00d994369ca6d1de71dc004f81ecf7afeace8f5f
yishen.sun <ethan.ys.sun@gmail.com>
2022-10-04 19:32:51 -0500

    fix

--- 2. ER Modeling & Schema Design/create.sql
+++ 2. ER Modeling & Schema Design/create.sql
@@ -4,8 +4,8 @@ CREATE TABLE Category (
 );
 
 CREATE TABLE Belong (
-    CategoryID      VARCHAR(255),
     ItemID          INTEGER,
+    CategoryID      VARCHAR(255),
     PRIMARY KEY (CategoryID, ItemID),
     FOREIGN KEY (CategoryID) REFERENCES Category (CategoryID),
     FOREIGN KEY (ItemID) REFERENCES Item (ItemID),
@@ -13,30 +13,30 @@ CREATE TABLE Belong (
 
 CREATE TABLE User (
     UserID          VARCHAR(255)    PRIMARY KEY,
-    Country         VARCHAR(255),
     Location        VARCHAR(255),
-    Rating          INTEGER         NOT NULL
+    Rating          INTEGER         NOT NULL,
+    Country         VARCHAR(255)
 );
 
 CREATE TABLE Item (
     ItemID          INTEGER         PRIMARY KEY,
-    Name            VARCHAR(255)    NOT NULL,
-    Description     VARCHAR(255)    NOT NULL,
+    Number_of_bids  INTEGER         NOT NULL,
     First_Bid       DOUBLE          NOT NULL,
     Buy_Price       DOUBLE
     Currently       DOUBLE          NOT NULL,
+    Name            VARCHAR(255)    NOT NULL,
+    Description     VARCHAR(255)    NOT NULL,
     Started         DATETIME        NOT NULL,
     Ends            DATETIME        NOT NULL,
-    Number_of_bids  INTEGER         NOT NULL,
     SellerID        VARCHAR(255),
     FOREIGN KEY (SellerID) REFERENCES User (UserID),
 );
 
 CREATE TABLE Bid (
-    UserID      VARCHAR(255)        NOT NULL,
     ItemID      INTEGER             NOT NULL,
-    Amount      VARCHAR(255)        NOT NULL,
+    UserID      VARCHAR(255)        NOT NULL,
     Time        DATETIME            NOT NULL,
+    Amount      VARCHAR(255)        NOT NULL,
     FOREIGN KEY (UserID) REFERENCES User (UserID),
     FOREIGN KEY (ItemID) REFERENCES Item (ItemID),
     PRIMARY KEY (UserID, ItemID, Time)
